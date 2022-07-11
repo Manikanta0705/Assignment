f95d9cc8d9531b53690e05f5ac173046b161175f
Manikanta0705 <102511255+Manikanta0705@users.noreply.github.com>
2022-07-11 08:10:51 +0530

    Update 1-1.c

--- 1-1.c
+++ 1-1.c
@@ -2,10 +2,16 @@
 #include <stdlib.h>
 #include <math.h>
 #include "source.h"
+
 int  main(void) //main function begins
 {
  
 //Uniform random numbers
-uniform("uni.dat", 1000000);
+//uniform("uni.dat", 1000000);
+
+//Gaussian random numbers
+gaussian("../data/gau1.dat", 1000000,1,1);
+
+
 return 0;
 }
